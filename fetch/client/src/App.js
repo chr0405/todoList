307bf5417bea8b665230ac379af61e86288b1e74
chr0405 <ch0nn@naver.com>
2023-12-30 16:27:32 +0900

    axios로 코드 수정

--- fetch/client/src/App.js
+++ fetch/client/src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 // 기본 제공하는 api 는 fetch
 /*
@@ -9,16 +10,31 @@ server 주소를 알아야 함
 
 // axios 는 라이브러리
 
+const SERVER_URL = 'http://localhost:4000/api/todo';
+
 function App() {
   const [TodoList, setTodoList] = useState(null);
 
-  // fetch로 받은 data 객체를 JS 객체로 프라미스로 변환 [파싱]
-  // 위에서 받은 프라미스의 객체 값을 출력
-  const fetchData = () => {
-    // method 아무것도 안 적으면 get 요청
-    fetch('http://localhost:4000/api/todo')
-    .then((response) => response.json())
-    .then((data) => setTodoList(data));
+  const fetchData = async () => {
+    // // method 아무것도 안 적으면 get 요청
+    // fetch(SERVER_URL)
+    // // fetch로 받은 data 객체를 JSON 객체로 변환 [파싱]
+    // .then((response) => response.json())
+    // // 위에서 받은 프라미스의 객체 값을 출력
+    // .then((data) => setTodoList(data));
+
+    // axios의 경우 get 요청도 명시
+
+    // // async를 사용하지 않는 경우
+    // axios.get(SERVER_URL)
+    // // JSON 객체로 변환 X
+    // .then((response) => {
+    //   setTodoList(response.data);
+    // });
+
+    // async를 사용하는 경우
+    const response = await axios.get(SERVER_URL);
+    setTodoList(response.data);
   } 
 
   useEffect(() => {
@@ -26,22 +42,27 @@ function App() {
   }, []);
   // useEffect에서 두 번째 인자에 아무것도 없으면 한 번만 실행 
   
-  const onSubmitHandler = (event) => {
+  const onSubmitHandler = async (event) => {
     event.preventDefault();
     const text = event.target.text.value;
     const done = event.target.done.checked;
-    fetch('http://localhost:4000/api/todo', {
-    // 데이터를 줄 때는 body에 문자열로 직렬화 해서
-    // method 에 'POST'라고 명시
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          text,
-          done,
-        }),
-      }).then(() => fetchData());
+
+    // fetch(SERVER_URL, {
+    // // 데이터를 줄 때는 body에 문자열로 직렬화 해서
+    // // method 에 'POST'라고 명시
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //       text,
+    //       done,
+    //     }),
+    //   }).then(() => fetchData());
+
+    // axios는 headers와 직렬화를 하지 않아도 O
+    await axios.post(SERVER_URL, {text, done});
+    fetchData();
   };
 
   return (
